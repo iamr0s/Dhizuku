1e7b8019745eff93b577f150fcdc686b908eaba0
Syuugo <pub@s1204.me>
2025-04-03 21:07:44 +0900

    Fix activity

--- app/src/main/java/com/rosan/dhizuku/ui/activity/FinalizeActivity.kt
+++ app/src/main/java/com/rosan/dhizuku/ui/activity/FinalizeActivity.kt
@@ -1,20 +1,42 @@
 package com.rosan.dhizuku.ui.activity
 
+import android.Manifest
 import android.app.Activity
 import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+
 import androidx.activity.ComponentActivity
+
 import com.rosan.dhizuku.shared.DhizukuVariables
+import com.rosan.dhizuku.server.DhizukuDAReceiver
+import com.rosan.dhizuku.R
+
 import org.koin.core.component.KoinComponent
 
+import rikka.shizuku.ShizukuProvider
+
 class FinalizeActivity : ComponentActivity(), KoinComponent {
+    val REQUIRE_PERMISSIONS: Array<String> = arrayOf(
+          Manifest.permission.READ_EXTERNAL_STORAGE,
+          Manifest.permission.WRITE_EXTERNAL_STORAGE,
+          Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+          DhizukuVariables.PERMISSION_API,
+          //ShizukuProvider.PERMISSION
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
-        do {} while(!devicePolicyManager.isDeviceOwnerApp(packageName))
-        devicePolicyManager.setProfileEnabled(DhizukuVariables.COMPONENT_NAME)
-        setResult(Activity.RESULT_OK, intent)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            REQUIRE_PERMISSIONS.forEach { permission ->
+                (getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager).setPermissionGrantState(ComponentName(this, DhizukuDAReceiver::class.java), DhizukuVariables.OFFICIAL_PACKAGE_NAME, permission, DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED)
+            }
+        }
+        setResult(Activity.RESULT_OK)
+        Toast.makeText(this, getString(R.string.home_status_owner_granted), Toast.LENGTH_LONG).show()
         finish()
     }
 }
--- app/src/main/java/com/rosan/dhizuku/ui/activity/ProvisioningActivity.kt
+++ app/src/main/java/com/rosan/dhizuku/ui/activity/ProvisioningActivity.kt
@@ -4,14 +4,16 @@ import android.app.Activity
 import android.app.admin.DevicePolicyManager
 import android.content.Intent
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
+
 import org.koin.core.component.KoinComponent
 
 class ProvisioningActivity : ComponentActivity(), KoinComponent {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent()
-        intent.putExtra("android.app.extra.PROVISIONING_MODE", DevicePolicyManager.PROVISIONING_MODE_FULLY_MANAGED_DEVICE)
+        intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_MODE, DevicePolicyManager.PROVISIONING_MODE_FULLY_MANAGED_DEVICE)
         setResult(Activity.RESULT_OK, intent)
         finish()
     }
