502ade5a6972c1f927abc347d70c1f9adb10dac0
Syuugo <pub@s1204.me>
2025-04-05 21:51:07 +0900

    Fix timing of toast

--- app/src/main/java/com/rosan/dhizuku/server/DhizukuDAReceiver.kt
+++ app/src/main/java/com/rosan/dhizuku/server/DhizukuDAReceiver.kt
@@ -8,6 +8,8 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.widget.Toast
+import com.rosan.dhizuku.R
 import com.rosan.dhizuku.shared.DhizukuVariables
 
 import org.koin.core.component.KoinComponent
@@ -23,16 +25,25 @@ class DhizukuDAReceiver : DeviceAdminReceiver(), KoinComponent {
         )
         fun grantPermissions(context: Context) {
             val dpm = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as? DevicePolicyManager
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && dpm!!.isDeviceOwnerApp(DhizukuVariables.OFFICIAL_PACKAGE_NAME)) {
-                requirePermissions.forEach { permission ->
-                    dpm.setPermissionGrantState(
-                        ComponentName(context, DhizukuDAReceiver::class.java),
-                        DhizukuVariables.OFFICIAL_PACKAGE_NAME,
-                        permission,
-                        DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
-                    )
+            if (dpm!!.isDeviceOwnerApp(DhizukuVariables.OFFICIAL_PACKAGE_NAME)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requirePermissions.forEach { permission ->
+                        dpm.setPermissionGrantState(
+                            ComponentName(context, DhizukuDAReceiver::class.java),
+                            DhizukuVariables.OFFICIAL_PACKAGE_NAME,
+                            permission,
+                            DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
+                        )
+
+                    }
                 }
+                Toast.makeText(
+                    context,
+                    context.getString(R.string.home_status_owner_granted),
+                    Toast.LENGTH_LONG
+                ).show()
             }
+
         }
     }
 
--- app/src/main/java/com/rosan/dhizuku/ui/activity/FinalizeActivity.kt
+++ app/src/main/java/com/rosan/dhizuku/ui/activity/FinalizeActivity.kt
@@ -1,12 +1,10 @@
 package com.rosan.dhizuku.ui.activity
 
 import android.os.Bundle
-import android.widget.Toast
 
 import androidx.activity.ComponentActivity
 
 import com.rosan.dhizuku.server.DhizukuDAReceiver
-import com.rosan.dhizuku.R
 
 import org.koin.core.component.KoinComponent
 
@@ -15,7 +13,6 @@ class FinalizeActivity : ComponentActivity(), KoinComponent {
         super.onCreate(savedInstanceState)
         DhizukuDAReceiver.grantPermissions(this)
         setResult(RESULT_OK)
-        Toast.makeText(this, getString(R.string.home_status_owner_granted), Toast.LENGTH_LONG).show()
         finish()
     }
 }
